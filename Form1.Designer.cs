9f71e921c763b339eba66e8f500b010e157f449d
ximo <ximo.coder@gmail.com>
2017-11-03 11:24:36 +0100

    First functional commit, need some refactor to allow different urls...

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WebtsToMp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(532, 256);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(514, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://35.dramaplace.com/hls/z_dnP8udiaTxWuj6lYfflaTg7p0pGFpSTmQ3vId5XC2lUBhzdcb3" +
+    "7Gp_QL9ikWMH/seg-1-v1-a1.ts";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(607, 209);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ts URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.Text = "WebTsToMp4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebtsToMp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            GetUrlData(this.textBox1.Text);
+        }
+
+        private void GetUrlData(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            var dataWithAllDatasMerged = new System.IO.MemoryStream();
+            string baseurl = url;
+
+            var status = ((System.Net.HttpWebResponse)response).StatusDescription;
+
+            int chunk = 1;
+
+            while (true)
+            {
+
+                url = baseurl.Substring(0, baseurl.IndexOf("seg-"))+ "seg-" + chunk.ToString() + "-v1-a1.ts";
+
+                HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpRequest.Method = "GET";
+
+                // if the URI doesn't exist, an exception will be thrown here...
+                using (HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse())
+                {
+                    using (Stream responseStream = httpResponse.GetResponseStream())
+                    {
+                        using (FileStream localFileStream =
+                            new FileStream(Path.Combine(@"C:\\tmp\\", @"tmp.mp4"), FileMode.Append))
+                        {
+                            var buffer = new byte[4096];
+                            long totalBytesRead = 0;
+                            int bytesRead;
+
+                            while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                totalBytesRead += bytesRead;
+                                localFileStream.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                    }
+                }
+
+                chunk += 1;
+
+            }
+            //while (status == "OK")
+            //{
+            //    Stream data;
+            //    data = response.GetResponseStream();
+            //    response.GetResponseStream().CopyTo(dataWithAllDatasMerged);
+
+            //    chunk += 1;
+            //    url = baseurl.Substring(0, baseurl.IndexOf("seg-")) + chunk.ToString() + "-v1-a1.ts";
+
+            //    request = WebRequest.Create(url);
+
+            //    try
+            //    {
+            //        response = request.GetResponse();
+            //    }
+            //    catch (Exception)
+            //    {
+            //        if (((System.Net.HttpWebResponse)response).StatusDescription != "OK")
+            //            return;
+            //    }
+
+
+            //}
+        }
+    }
+}
